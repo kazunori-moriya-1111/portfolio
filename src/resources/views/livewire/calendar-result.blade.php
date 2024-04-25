4ce3001a32010c4dbd03e9760681131609e4580e
kazunori_moriya <g.ff.k.96@gmail.com>
2024-04-25 15:27:53 +0900

    不要な表示を削除

--- src/resources/views/livewire/calendar-result.blade.php
+++ src/resources/views/livewire/calendar-result.blade.php
@@ -2,8 +2,6 @@
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                {{ $year }}
-                {{ $month }}
                 <th scope="col" class="px-6 py-3">
                     総掛け金
                 </th>
