49b55dc3811dedbfd2322cbdacadbd74c0076ee0
kazunori_moriya <g.ff.k.96@gmail.com>
2024-05-05 20:52:41 +0900

    typo fix

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/css/calendar.css',
-                'resources/js/app.js',
-                'resources/js/chartjs.js',
-                'resources/js/calendar.js',
+                "resources/css/app.css",
+                "resources/css/calender.css",
+                "resources/js/app.js",
+                "resources/js/chartjs.js",
+                "resources/js/calendar.js",
             ],
             refresh: true,
         }),
