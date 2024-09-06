3d7d9686c77e2104d361a0012180528c69a8c031
kazunori_moriya <g.ff.k.96@gmail.com>
2024-09-06 16:02:13 +0900

    ログイン画面のUIを修正

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-text-input id="email" class="block p-1 mt-1 w-full border" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
@@ -16,7 +16,7 @@
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
+            <x-text-input id="password" class="block p-1 mt-1 w-full border"
                             type="password"
                             name="password"
                             required autocomplete="current-password" />
@@ -25,35 +25,35 @@
         </div>
 
         <!-- Remember Me -->
-        <div class="block mt-4">
+        <!-- <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
                 <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
             </label>
-        </div>
+        </div> -->
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex justify-between items-center mt-4">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
-
-            <x-primary-button class="ml-3">
+            <x-primary-button class="m-1">
                 {{ __('Log in') }}
+            </x-primary-button>
+            <x-primary-button class="m-1" onclick="location.href='./register'">
+                {{ __('Sign in')}}
             </x-primary-button>
         </div>
     </form>
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-        <x-primary-button class="ml-3">
-            <input type="hidden" name="email" value="test@test.com">
-            <input type="hidden" name="password" value="password">
-            {{ __('test_user Log in')}}
-        </x-primary-button>
-    </form>
-    <button class="ml-3" type="button" onclick="location.href='./register'">
-            {{ __('CREATE_USER')}}
-    </button>
-    <a href="{{ route('login-migrate') }}">DB migrate</a>
+    <div class="mt-1 flex flex-row-reverse ">
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+            <x-primary-button class="ml-3">
+                <input type="hidden" name="email" value="test@test.com">
+                <input type="hidden" name="password" value="password">
+                {{ __('test_user Log in')}}
+            </x-primary-button>
+        </form>
+    </div>
 </x-guest-layout>
