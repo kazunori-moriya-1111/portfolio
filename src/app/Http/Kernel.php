fcc076fa6b6a433e23d0255e0c9c1cf475699684
kazunori_moriya <g.ff.k.96@gmail.com>
2024-09-07 15:08:01 +0900

    middlewareでtest_userがprofileページへアクセスできないように制御

--- src/app/Http/Kernel.php
+++ src/app/Http/Kernel.php
@@ -63,5 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        // テストユーザアクセス制御用のmiddleware
+        'restrictTestUserAccess' => \App\Http\Middleware\RestrictTestUserAccess::class,
     ];
 }
