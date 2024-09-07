9d9b3d938a3581178a130ba88791de4aafa74870
kazunori_moriya <g.ff.k.96@gmail.com>
2024-09-07 14:27:51 +0900

    test_userの時はprofileへのリンクを表示させないように修正

--- src/resources/views/components/nav-bar.blade.php
+++ src/resources/views/components/nav-bar.blade.php
@@ -15,9 +15,11 @@
                     </button>
                 </x-slot>
                 <x-slot name="content">
-                    <x-dropdown-link :href="route('profile.edit')">
-                        {{ __('Profile') }}
-                    </x-dropdown-link>
+                    @if( Auth::user()->name !== 'test_user')
+                        <x-dropdown-link :href="route('profile.edit')">
+                            {{ __('Profile') }}
+                        </x-dropdown-link>
+                    @endif
                     <!-- Authentication -->
                     <form method="POST" action="{{ route('logout') }}">
                         @csrf
