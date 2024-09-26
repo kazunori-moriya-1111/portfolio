8e63110a0f9367d9cb0bf5737c745d2320307c60
kazunori_moriya <g.ff.k.96@gmail.com>
2024-09-26 11:12:15 +0900

    IAMリソース作成

---
+++ terraform/iam/main.tf
@@ -0,0 +1,96 @@
+resource "aws_iam_role" "iac-ecsInstanceRole" {
+  name = "iac-ecsInstanceRole"
+  assume_role_policy = jsonencode({
+    Version = "2008-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+  managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"]
+}
+
+resource "aws_iam_role" "iac-ecsRole" {
+  name = "iac-ecsRole"
+  assume_role_policy = jsonencode({
+    Version = "2008-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+  managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"]
+}
+
+resource "aws_iam_role" "iac-ecsAutoScalingRole" {
+  name = "iac-ecsAutoScalingRole"
+  assume_role_policy = jsonencode({
+    Version = "2008-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+  managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceAutoscaleRole"]
+}
+
+resource "aws_iam_role" "iac-ecsTaskExecutionRole" {
+  name = "iac-ecsTaskExecutionRole"
+  assume_role_policy = jsonencode({
+    Version = "2008-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+  managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"]
+}
+
+
+resource "aws_iam_role_policy" "iac-ecsFargateExecPolicy" {
+  role = aws_iam_role.iac-ecsTaskExecutionRole.name
+  name = "iac-ecsFargateExecPolicy"
+  policy = data.aws_iam_policy_document.iac-ecsFargateExecPolicy.json
+}
+
+data "aws_iam_policy_document" "iac-ecsFargateExecPolicy"{
+  statement {
+    effect = "Allow"
+    actions = [
+      "ssmmessages:CreateControlChannel",
+      "ssmmessages:CreateDataChannel",
+      "ssmmessages:OpenControlChannel",
+      "ssmmessages:OpenDataChannel"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policies_exclusive" "name" {
+  role_name = aws_iam_role.iac-ecsTaskExecutionRole.name
+  policy_names = [aws_iam_role_policy.iac-ecsFargateExecPolicy.name]
+}
---
+++ terraform/main.tf
@@ -0,0 +1,9 @@
+# Configure the AWS Provider
+provider "aws" {
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
+}
+
+module "iam" {
+  source      = "./iam"
+}
