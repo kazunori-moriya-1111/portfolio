941b772ff3097e1aa1ff81de724a8fe0bf22bbf2
kazunori_moriya <g.ff.k.96@gmail.com>
2024-10-09 20:05:59 +0900

    bladeに適用するjs,cssを選択できるように修正

--- src/resources/views/components/vite.blade.php
+++ src/resources/views/components/vite.blade.php
@@ -1,20 +1,53 @@
+@props(['files'])
+
 @if(config('app.env') == 'local')
     <!-- 検証環境 -->
-    @vite('resources/css/app.css')
+    @foreach($files as $file)
+        @php
+            $css_file_path = resource_path() . "/css/$file*.css";
+            $css_files = glob($css_file_path);
+        @endphp
+
+        @foreach($css_files as $css_file)
+            @if($css_file)
+                @vite("resources/css/$file.css")
+            @endif
+        @endforeach
+        
+        @php
+            $js_file_path = resource_path() . "/js/$file*.js";
+            $js_files = glob($js_file_path);
+        @endphp  
+        
+        @foreach($js_files as $js_file)
+            @if($js_file)
+                @vite("resources/js/$file.js")
+            @endif
+        @endforeach
+    @endforeach
 @else
     <!-- 本番環境 -->
-    @php
-        $css_file_path = public_path() . '/build/assets/*.css';
-        $css_files = glob($css_file_path);
-    @endphp  
-    @foreach($css_files as $css_file)
-        <link rel="stylesheet" href="{{ asset('build/assets/' . basename($css_file)) }}"/>
-    @endforeach
-    @php
-        $js_file_path = public_path() . '/build/assets/*.js';
-        $js_files = glob($js_file_path);
-    @endphp  
-    @foreach($js_files as $js_file)
-        <script type="module" src="{{ asset('build/assets/' . basename($js_file)) }}"></script>
+    @foreach($files as $file)
+        @php
+            $css_file_path = public_path() . "/build/assets/$file*.css";
+            $css_files = glob($css_file_path);
+        @endphp
+
+        @foreach($css_files as $css_file)
+            @if($css_file)
+                <link rel="stylesheet" href="{{ asset('build/assets/' . basename($css_file)) }}"/>
+            @endif
+        @endforeach
+        
+        @php
+            $js_file_path = public_path() . "/build/assets/$file*.js";
+            $js_files = glob($js_file_path);
+        @endphp  
+        
+        @foreach($js_files as $js_file)
+            @if($js_file)
+                <script type="module" src="{{ asset('build/assets/' . basename($js_file)) }}"></script>
+            @endif
+        @endforeach
     @endforeach
 @endif
